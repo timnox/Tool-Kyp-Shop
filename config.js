f50d930290314ea952fdb393e18bb8095da6333a
Timnox <ilyesfougani@gmail.com>
2025-05-05 20:18:41 +0200

    Fix COLOR env and config.js logic

--- config.js
+++ config.js
@@ -1,14 +1,13 @@
 module.exports = {
-   token: process.env.TOKEN,
-   owner: process.env.OWNER_IDS?.split(',') || [],
-   color: process.env.COLOR || "#FF0000",
-   statut: process.env.STATUT || "Mon statut",
-   activity: process.env.ACTIVITY || "playing",
-   status: process.env.STATUS || "online",
-   vouchlogs: process.env.VOUCH_LOGS_ID,
-   suggestionlogs: process.env.SUGGESTION_LOGS_ID,
-   customer: process.env.CUSTOMER_ROLE_ID,
-   bot: process.env.BOT_IDS?.split(',') || [],
-   channelId: process.env.CHANNEL_ID
- };
- 
+  token: process.env.TOKEN,
+  owner: process.env.OWNER_IDS?.split(',') || [],
+  color: "#" + (process.env.COLOR || "FF0000"), // <-- ajout du # ici
+  statut: process.env.STATUT || "Mon statut",
+  activity: process.env.ACTIVITY || "playing",
+  status: process.env.STATUS || "online",
+  vouchlogs: process.env.VOUCH_LOGS_ID,
+  suggestionlogs: process.env.SUGGESTION_LOGS_ID,
+  customer: process.env.CUSTOMER_ROLE_ID,
+  bot: process.env.BOT_IDS?.split(',') || [],
+  channelId: process.env.CHANNEL_ID
+};
